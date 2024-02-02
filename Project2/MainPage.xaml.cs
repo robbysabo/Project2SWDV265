50654b045c2870437c2013e79e1900891602464e
Robby Sabo <robertalensabo@gmail.com>
2024-02-02 15:57:10 -0700

    first commit

---
+++ Project2/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Project2.ViewModel;
+
+namespace Project2
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new Project2Viewmodel();
+        }
+    }
+
+}
---
+++ Project2/ViewModel/Project2Viewmodel.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace Project2.ViewModel;
+
+public class Project2Viewmodel : INotifyPropertyChanged
+{
+    public string Title { get; set; } = "Robert Sabo : Project 2";
+
+    private string _myname = "Robert Sabo";
+    public string MyName
+    {
+        get => _myname;
+        private set
+        {
+            _myname = value;
+
+            OnPropertyChanged();
+        }
+    }
+
+    private string _ChangeButtonText = "Show first name";
+    public string ChangeButtonText
+    {
+        get => _ChangeButtonText;
+        set
+        {
+            if (_myname == "Robert")
+            {
+                _ChangeButtonText = "Show first name";
+                MyName = "Robert Sabo";
+            } else
+            {
+                MyName = "Robert";
+                _ChangeButtonText = "Show full name";
+            }
+            OnPropertyChanged();
+        }
+    }
+
+    public ICommand OnChangeButtonTextClicked
+    {
+        get;
+        private set;
+    }
+
+
+    private string _TopBottomText = "Top";
+    public string TopBottomText
+    {
+        get => _TopBottomText;
+        private set
+        {
+            _TopBottomText = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private bool _ChangeTopBottomText = true;
+    public bool ChangeTopBottomText
+    {
+        get => _ChangeTopBottomText;
+        set
+        {
+            _ChangeTopBottomText = value;
+            if (value)
+            {
+                TopBottomText = "Top";
+            } else
+            {
+                TopBottomText = "Bottom";
+            }
+            OnPropertyChanged();
+        }
+    }
+
+    public Project2Viewmodel()
+    {
+        OnChangeButtonTextClicked = new Command<string>((key) => ChangeButtonText="change");
+    }
+
+    public void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+}
+
